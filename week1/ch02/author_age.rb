5eaab189d82a36cb80b3fc8565116021a18871cd
J3nni <jennifermartinez@berkeley.edu>
2018-06-14 18:34:03 -0700

    Complete author_age exercise

--- week1/ch02/author_age.rb
+++ week1/ch02/author_age.rb
@@ -1,2 +1,3 @@
 puts 'If the author is about ' +(1.160).to_s+ ' million seconds old, how old is he?'
-puts 'Well if the author is about ' +(1.160).to_s+ ' million seconds old, the author is about ' +(1160000000 / 365.25 / 24 / 60 / 60).to_i+ ' years old.'
+age = (1160000000 / 365.25 / 24 / 60 / 60).to_i
+puts 'Well if the author is about ' +(1.160).to_s+ ' million seconds old, the author is about ' + age + ' years old.'

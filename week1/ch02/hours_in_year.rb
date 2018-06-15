390d1b7fcde3e47ae2850891c60b960a7cdf5b92
J3nni <jennifermartinez@berkeley.edu>
2018-06-14 18:46:41 -0700

    Complete hours_in_year exercise

--- week1/ch02/hours_in_year.rb
+++ week1/ch02/hours_in_year.rb
@@ -1,2 +1,2 @@
 puts 'There are ' +(24 * 365).to_s+ ' hours in a year.'
-puts 'In a leap year, there are ' +(24 * 366).to_s+ ' hours.'
+puts 'Since a leap year carries an extra day, there are ' +(24 * 366).to_s+ ' hours in a leap year!'

0679b3ed11ce9097cc89abeeefe52ef62e91c3dc
J3nni <jennifermartinez@berkeley.edu>
2018-06-14 19:29:19 -0700

    Complete full_name_greeting exercise

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -1,13 +1,13 @@
 puts 'Hello there. and what is your name?'
 name1 = gets.chomp
-end-of-input
+
 puts 'Your name is ' + name1 + '? What a lovely name!'
 puts 'Well ' + name1 + ' what is your middle name?'
 name2 = gets.chomp
-end-of-input
+
 puts 'Wow!' + name2 + ' is a cool middle name!'
 puts 'I wish I had a cool middle name like that.'
 puts 'One last question, what is your last name ' + name1 + '.'
 name3 = gets.chomp
-end-of-input
+
 puts 'Pleased to meet you, ' + name1 + ' ' + name2 + ' ' + name3 + '.'

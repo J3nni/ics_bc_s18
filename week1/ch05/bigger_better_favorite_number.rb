2aa1be356b812a8b0f7aed1fa5a98f7a06e83dd5
J3nni <jennifermartinez@berkeley.edu>
2018-06-15 18:06:01 -0700

    Complete week1_ch05 exercise

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -1,6 +1,7 @@
 puts 'Hello there!'
 puts 'I am curious about something, so I have a question to ask.'
 puts 'What is your favorite number in the ENTIRE WORLD?'
+$stdout.flush
 number = gets.chomp.to_s
 puts 'So your favorite number is ' + number + '?'
 puts 'Well I have a bigger and better number just for you.'

cde7020492ed053aba19450a84b0a55fdd5442a5
J3nni <jennifermartinez@berkeley.edu>
2018-06-17 21:13:54 -0700

    Complete age_in_seconds exercise

--- week1/ch02/age_in_seconds.rb
+++ week1/ch02/age_in_seconds.rb
@@ -1,2 +1,2 @@
 puts 'My birthday is on February 19th, 2018.'
-puts 'So as of June 14th, 2018, I am about ' +(60 * 60 * 24 * 365.25 * 18 + 60 * 60 * 24 * 115).to_s+ ' seconds old.'
+puts 'So as of June 14th, 2018, I am about ' + (60 * 60 * 24 * 365.25 * 18 + 60 * 60 * 24 * 115).to_s + ' seconds old.'

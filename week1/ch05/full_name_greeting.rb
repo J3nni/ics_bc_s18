718bd7059c79cf5a131e7bee445cb7d6fb9cbb07
J3nni <jennifermartinez@berkeley.edu>
2018-06-17 21:21:10 -0700

    Complete full_name_greeting exercise

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -1,13 +1,13 @@
-puts 'Hello there. and what is your name?'
+puts 'Hello there, and what is your name?'
 $stdout.flush
 name1 = gets.chomp
 puts 'Your name is ' + name1 + '? What a lovely name!'
 puts 'Well ' + name1 + ' what is your middle name?'
 $stdout.flush
 name2 = gets.chomp
-puts 'Wow!' + name2 + ' is a cool middle name!'
+puts 'Wow! ' + name2 + ' is a cool middle name!'
 puts 'I wish I had a cool middle name like that.'
 puts 'One last question, what is your last name ' + name1 + '.'
-$stdout.flush 
+$stdout.flush
 name3 = gets.chomp
 puts 'Pleased to meet you, ' + name1 + ' ' + name2 + ' ' + name3 + '.'

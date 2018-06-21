921124fea98487b18ebe7452ecbd2cac5ff3685e
J3nni <jennifermartinez@berkeley.edu>
2018-06-20 17:32:13 -0700

    Complete angry_boss.rb exercise

---
+++ week2/ch06/angry_boss.rb
@@ -0,0 +1,6 @@
+puts "Hey boss. Sorry for interrupting, but I need to ask you something?"
+$stdout.flush
+asks = gets.chomp.capitalize
+puts "WHAT DO YOU MEAN "" + asks.upcase + ""?1? ARE YOU CRAZY!!!"
+puts "YOU KNOW WHAT... YOU ARE F I R E D!!!"
+puts "LEAVE MY OFFICE IMMEDIATELY!!!"

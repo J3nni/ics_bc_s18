37af8c77207ed5a599351ead96987b24345e1c1d
J3nni <jennifermartinez@berkeley.edu>
2018-06-20 19:30:56 -0700

    Complete table_of_contents exercise

--- week2/ch06/table_of_contents.rb
+++ week2/ch06/table_of_contents.rb
@@ -1,10 +1,10 @@
 #Set page width
    page_width = 60
- #Set Title. Its at 0 because arrays start at 0
+ #Set Title
    puts ("Table of Contents").center(page_width/2)
  #Make a gap
    puts ''
- #Setting the sections left justified and right justified.
+ #Setting the sections left justified and right justified
    puts ("Chapter 1: Getting Started".ljust(page_width/2) +  "page  1".rjust(page_width/2))
    puts ("Chapter 2: Numbers".ljust(page_width/2) +  "page  9".rjust(page_width/2))
    puts ("Chapter 3: Letters".ljust(page_width/2) +  "page 13".rjust(page_width/2))

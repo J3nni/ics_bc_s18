39c558a30c08432cec360167530e443f69a3abbd
J3nni <jennifermartinez@berkeley.edu>
2018-06-20 20:16:48 -0700

    Complete 99_bob exercise

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -8,6 +8,8 @@ while bottles_of_beer > 1 do
   puts ""
   if
     bottles_of_beer == 1
+    puts "1 bottle of beer on the wall, 1 bottle of beer."
+    puts "Take one down and pass it around, no more bottles of beer on the wall."
     puts "No more bottle of beer on the wall, no more bottles of beer."
     puts "Go to the store and buy some more, 99 bottles of beer on the wall."
   end

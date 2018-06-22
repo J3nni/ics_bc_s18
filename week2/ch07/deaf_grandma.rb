86db56be14df15490710a95d162400f95edd863c
J3nni <jennifermartinez@berkeley.edu>
2018-06-21 17:49:54 -0700

    Complete deaf_grandma exercise

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -2,14 +2,18 @@ puts "Say something nice to Grandma."
 puts " "
 puts "Hey Sonny! It\'s your Grandma! How are you?"
 bye = 0
-while (response = gets.chomp) != "BYE"
+while bye < 3
+  response = gets.chomp
   if response != response.upcase
     puts "Huh?! I CAN\'T HEAR YOU!"
     puts "SPEAK UP, SWEETIE"
   else
     puts "NO! NOT SINCE " + (1930 + rand(21)).to_s + "!"
   end
-  if bye == "BYE"
+elsif
+   response == "BYE"
+    puts "STAY AWHILE!?!"
+    bye = (bye+1)
   end
 end
 puts "PLEASE VISIT ME AGAIN!"

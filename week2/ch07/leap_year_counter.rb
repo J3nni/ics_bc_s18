942f115b6d87b422381d67068834c28f2b7c2c01
J3nni <jennifermartinez@berkeley.edu>
2018-06-21 19:33:16 -0700

    Complete leap_year_counter exercise

--- week2/ch07/leap_year_counter.rb
+++ week2/ch07/leap_year_counter.rb
@@ -11,8 +11,8 @@ if endyear < beginyear
         puts 'Note: Begin year < End year'
 else
         while (beginyear <= endyear)
-                if (((beginyear %4 == 0)
-                  if (beginyear %100 !=0)) || + (beginyear %400 == 0))
+                if
+                  (((beginyear %4 == 0) && (beginyear %100 !=0)) || + (beginyear %400 == 0))
        puts beginyear.to_s + "is a leap year"
      end
      (beginyear = beginyear.to_i + 1)

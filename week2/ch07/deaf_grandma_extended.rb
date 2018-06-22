cb0385d93f699190184e331ba0aaf5c3454e9b8f
J3nni <jennifermartinez@berkeley.edu>
2018-06-21 18:58:15 -0700

    Complete deaf_grandma_extended exercise

--- week2/ch07/deaf_grandma_extended.rb
+++ week2/ch07/deaf_grandma_extended.rb
@@ -2,21 +2,17 @@ puts "Say something nice to Grandma."
 puts " "
 puts "Hey Sonny! It\'s your Grandma! How are you?"
 bye = 0
-$stdout.flush
 while bye < 3
     puts "What do you want to say to Grandma?"
-    $stdout.flush
     tell_grandma = gets.chomp
     if tell_grandma == "BYE"
         bye += 1
     end
-    $stdout.flush
     if tell_grandma == tell_grandma.upcase
       puts "NO, NOT SINCE " + (1930 + rand(21)).to_s + "!"
     else
         puts "HUH!? I CAN\'T HEAR YOU! SPEAK UP, SONNY!"
     end
-    $stdout.flush
 end
 while bye == 3
     puts "BYE BYE, SONNY!"

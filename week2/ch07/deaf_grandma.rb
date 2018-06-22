210aea8768a748cbe6a4c3c72ce6f9fffc36775b
J3nni <jennifermartinez@berkeley.edu>
2018-06-21 18:22:13 -0700

    Complete deaf_grandma exercise

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -5,17 +5,14 @@ bye = 0
 reply = gets.chomp
 if reply == "BYE"
   bye += 1
-  when 1; puts "CAN\'T HEAR YOU, DEAR!"
+  end_of_input when 1; puts "CAN\'T HEAR YOU, DEAR!"
           puts "SPEAK UP, SWEETIE"
-  end
-  when 2; puts "STAY AWHILE!?!"
-  end
+  end_of_input when 2; puts "STAY AWHILE!?!"
   else;   puts "PLEASE VISIT ME AGAIN!"
           puts "COME BACK SOON HONEY!"
           puts "GOOD BYE, SONNY!"
           puts "TAKE CARE!"]
   end
-end
 else
   bye = 0
   if reply = reply.upcase

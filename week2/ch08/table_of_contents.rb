43e2cc721778cea1d12076698a801b95fc6ef920
J3nni <jennifermartinez@berkeley.edu>
2018-06-24 20:52:31 -0700

    Complete table_of_contents exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,5 +1,5 @@
 # Array of table of contents, including title
-toc = ['Table of Contents', 'Chapter 1: Getting Started', 'page  1' , 'Chapter 2: Numbers', 'page  9', 'Chapter 3: Letters', 'page 13']
+toc = ['Table of Contents', 'Chapter 1: Getting Started'], ['page  1' , 'Chapter 2: Numbers', 'page  9', 'Chapter 3: Letters', 'page 13']
 # Set page width
 page_width = 60
 i = 0

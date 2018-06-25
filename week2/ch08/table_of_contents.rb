f7cb1d055ef9bd8971b10c1ba99f9dd0ddd92c51
J3nni <jennifermartinez@berkeley.edu>
2018-06-24 20:25:00 -0700

    Complete table_of_contents exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -9,6 +9,6 @@ puts (toc[0]).center(page_width)
 # Make a gap
 puts
 # Setting the sections left justified and right justified.
-until t == 3
-  puts (toc[t += 0].ljust(page_width/2) + pg[t += 0].rjust(page_width/2))
+until t == 2
+  puts (toc[t += 1].ljust(page_width/2) + pg[t += 0].rjust(page_width/2))
 end

eb66c138b0c78d420b55c33e6d2c257fade3440a
J3nni <jennifermartinez@berkeley.edu>
2018-06-24 20:15:26 -0700

    Complete table_of_contents exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -3,12 +3,14 @@ toc = ['Table of Contents', 'Chapter 1: Getting Started', 'Chapter 2: Numbers', 
 pg = ['page  1', 'page  9', 'page 13']
 # Set page width
 page_width = 60
-i = 0
+t = 0
+p
 # Set Title. Its at 0 because arrays start at 0
 puts (toc[0]).center(page_width)
 # Make a gap
 puts
 # Setting the sections left justified and right justified.
 until i == 3
-puts (toc[i += 1].ljust(page_width/2) + pg[i += 1].rjust(page_width/2))
+until p == 2
+puts (toc[i += 1].ljust(page_width/2) + pg[p += 0].rjust(page_width/2))
 end

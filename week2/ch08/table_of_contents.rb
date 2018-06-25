b44114003d51cee0f6a829e4826fc88d892ca6a2
J3nni <jennifermartinez@berkeley.edu>
2018-06-24 21:13:53 -0700

    Complete table_of_contents exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,7 +1,6 @@
 # Array of table of contents, including title
-toc = ['Table of Contents']
-ch = ['Chapter 1: Getting Started', 'Chapter 2: Numbers', 'Chapter 3: Letters']
-pg = ['page  1', 'page  9', 'page 13']
+toc = ['Table of Contents', 'Chapter 1: Getting Started', 'Chapter 2: Numbers', 'Chapter 3: Letters']
+pg = ['i', 'page  1', 'page  9', 'page 13']
 # Set page width
 page_width = 60
 i = 0
@@ -11,5 +10,5 @@ puts (toc[0]).center(page_width)
 puts
 # Setting the sections left justified and right justified.
 until i == 3
-puts (ch[i = 1].ljust(page_width/2) + pg[i = 1].rjust(page_width/2))
+puts (toc[i += 1].ljust(page_width/2) + pg[i += 0].rjust(page_width/2))
 end

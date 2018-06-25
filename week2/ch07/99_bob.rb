4558b0de5aafd3543cea8b67bb6e544d79a22d81
J3nni <jennifermartinez@berkeley.edu>
2018-06-24 18:40:44 -0700

    Complete 99_bob.rb exercise

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -1,16 +1,15 @@
-bottles_of_beer = 99
-while bottles_of_beer > 1 do
-  puts bottles_of_beer.to_s + " bottles of beer on the wall, " +
-  bottles_of_beer.to_s + " bottles of beer"
-  bottles_of_beer = bottles_of_beer - 1
-  puts "You take one down, pass it around, " + bottles_of_beer.to_s +
+bottles = 99
+while bottles > 1 do
+  puts bottles.to_s + " bottles of beer on the wall, " +
+  bottles.to_s + " bottles of beer"
+  bottles = bottles - 1
+  puts "You take one down, pass it around, " + bottles.to_s +
   " bottles of beer on the wall."
-  puts ""
-  if
-    bottles_of_beer == 1
+  puts
+  if bottles == 1
     puts "1 bottle of beer on the wall, 1 bottle of beer."
     puts "Take one down and pass it around, no more bottles of beer on the wall."
-    puts ""
+    puts
     puts "No more bottle of beer on the wall, no more bottles of beer."
     puts "Go to the store and buy some more, 99 bottles of beer on the wall."
   end

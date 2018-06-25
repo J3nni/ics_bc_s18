0218c3671ed69465a34ec76b47f430f136f9fd02
J3nni <jennifermartinez@berkeley.edu>
2018-06-24 21:46:38 -0700

    Complete table_of_contents exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,7 +1,7 @@
 # Array of table of contents, including title
 toc = 'Table of Contents'
-ch = ['Chapter 1: Getting Started', 'Chapter 2: Numbers', 'Chapter 3: Letters']
-pg = ['page  1', 'page  9', 'page 13']
+ch = ['Chapter 1: Getting Started', 'Chapter 2: Numbers', 'Chapter 3: Letters'].each_with_index
+pg = ['page  1', 'page  9', 'page 13'].each_with_index
 # Set page width
 page_width = 60
 # Set Title. Its at 0 because arrays start at 0
@@ -12,6 +12,6 @@ puts
 i = 0
 while i < ch.length && pg.length
   until i == 2
-    puts (ch[each_with_index].ljust(page_width/2) + pg[each_with_index].rjust(page_width/2))
+    puts (ch[i].ljust(page_width/2) + pg[i].rjust(page_width/2))
   end
 end

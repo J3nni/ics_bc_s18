9153a53b18fc533dcd9de790f764fecbf0533e95
J3nni <jennifermartinez@berkeley.edu>
2018-06-28 18:35:53 -0700

    Complete ask.rb exercise

--- week3/ch09/ask.rb
+++ week3/ch09/ask.rb
@@ -16,7 +16,7 @@ puts "Hello, and thank you for..."
 puts
 ask "Do you like eating tacos?"
 ask "Do you like eating burritos?"
-wet_bed = ask "Do you wet the bed?"
+wets_bed = ask "Do you wet the bed?"
 ask "Do you like eating chimichangas?"
 ask "Do you like eating sopapillas?"
 puts "Just a few more questions..."

0477269e68ceb3fe0c615dfa97a2ea618efa47e4
J3nni <jennifermartinez@berkeley.edu>
2018-07-05 19:52:52 -0700

    Complete english_number exercise

--- week4/ch10/english_number.rb
+++ week4/ch10/english_number.rb
@@ -2,7 +2,7 @@ def english_number(number)
   return 'Please enter a number that isn\'t negative.' if number < 0 # No negative numbers.
   return 'zero' if number == 0
   # No more special cases! No more returns!
-  num_string = '' # This is the string we will return.
+  num_string = "" # This is the string we will return.
   ones_place = %w[one two three four five six
                   seven eight nine]
   tens_place = %w[ten twenty thirty forty fifty
@@ -15,21 +15,57 @@ def english_number(number)
   # writing out right now.
   # write and left...get it? :)
   left = number
+  write = left / 1000000000000
+  left = left - write * 1000000000000
+  if write > 0
+    trillions = english_number write
+    num_string = num_string + trillions + " trillion"
+    if left > 0
+      num_string = num_string + " "
+    end
+  end
+  write = left / 1000000000
+  left = left - write * 1000000000
+  if write > 0
+    billions = english_number write
+    num_string = num_string + billions + " billion"
+    if left > 0
+      num_string = num_string + " "
+    end
+  end
+  write = left / 1000000
+  left = left - write * 1000000
+  if write > 0
+    millions = english_number write
+    num_string = num_string + millions + " million"
+    if left > 0
+      num_string = num_string + " "
+    end
+  end
+  write = left / 1000
+  left = left - write * 1000
+  if write > 0
+    thousands = english_number write
+    num_string = num_string + thousands + " thousand"
+    if left > 0
+      num_string = num_string + " "
+    end
+  end
   write = left / 100 # How many hundreds left?
   left -= write * 100 # Subtract off those hundreds.
   if write > 0
     # Now here's the recursion:
     hundreds = english_number write
-    num_string = num_string + hundreds + ' hundred'
+    num_string = num_string + hundreds + " hundred"
     if left > 0
       # So we don't write 'two hundredfifty-one'...
-      num_string += ' '
+      num_string += " "
     end
   end
   write = left / 10 # How many tens left?
   left -= write * 10 # Subtract off those tens.
   if write > 0
-    if (write == 1) && (left > 0)
+    if ((write == 1) && (left > 0))
       # Since we can't write "tenty-two" instead of
       # "twelve", we have to make a special exception
       # for these.
@@ -46,7 +82,7 @@ def english_number(number)
     end
     if left > 0
       # So we don't write 'sixtyfour'...
-      num_string += '-'
+      num_string += "-"
     end
   end
   write = left # How many ones left to write out?

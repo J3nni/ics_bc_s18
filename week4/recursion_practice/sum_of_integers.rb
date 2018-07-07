c89f9628885faf22534ec13e98a122ecc25cc5cd
J3nni <jennifermartinez@berkeley.edu>
2018-07-06 17:03:07 -0700

    Complete sum_of_integers exercise

--- week4/recursion_practice/sum_of_integers.rb
+++ week4/recursion_practice/sum_of_integers.rb
@@ -1,9 +1,9 @@
 def sum_of_integers(n)
-  if ______________________
-    n
+  if (n == 1)
+    return 1
   else
-    ______________________
+    return n + sum_of_integers(n - 1)
   end
 end
 
-puts sum_of_integers __
+puts sum_of_integers(10)

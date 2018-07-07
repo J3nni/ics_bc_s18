1103b1052d64d632890caeefaa5cdaf60ff7189a
J3nni <jennifermartinez@berkeley.edu>
2018-07-06 17:51:17 -0700

    Complete array_sum.rb

--- week4/recursion_practice/array_sum.rb
+++ week4/recursion_practice/array_sum.rb
@@ -1,9 +1,11 @@
 def array_sum(array)
-  if ______________________
+  if array.empty?
     array[0]
   else
-    ______________________
+    array[0] = array.pop + array[0];
+    array_sum(array) until array.length == 1
+    array.first
   end
 end
 
-puts array_sum [__, __, ...] 
+puts(array_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))

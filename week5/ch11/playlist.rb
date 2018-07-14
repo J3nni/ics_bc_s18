fd6b9f6da5993a772e9a90839c5e3be0320f19b4
J3nni <jennifermartinez@berkeley.edu>
2018-07-13 22:25:17 -0700

    Complete playlist exercise

--- week5/ch11/playlist.rb
+++ week5/ch11/playlist.rb
@@ -1,4 +1,36 @@
-### Copy in your shuffle method first
+def shuffle arr
+  shuf = []
+  while arr.length > 0
+    rand_index = rand(arr.length)
+    curr_index = 0
+    new_arr = []
+    arr.each do |item|
+      if curr_index == rand_index
+        shuf.push item
+      else
+        new_arr.push item
+      end
+      curr_index = curr_index + 1
+    end
+    arr = new_arr
+  end
+  shuf
+end
+
+Dir.chdir 'C:/Users/User/code/ics_bc_s18/week5/ch11/music'
+
+filename = 'music'
+song_names = shuffle(Dir['**/*.mp3'])
+
+puts "Searching for files:"
+
+print "Adding #{song_names.length} files: "
 
 
-### Now use it to build your playlist:
+File.open filename, 'w' do |f|
+  song_names.each do |song|
+    f.write song + "\n"
+  end
+end
+
+puts "Done!"
